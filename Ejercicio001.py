ff39e08df1fdf4b1c850f1904b3d41897c26e1d6
Gabo023 <bryangabo023@gmail.com>
2024-03-03 21:58:59 -0500

    Añadir los archivos

---
+++ Ejercicio001.py
@@ -0,0 +1,6 @@
+anio = int(input("Ingrese el año actual: "))
+edad = int(input("Ingrese el año de nacimiento: "))
+
+res = anio - edad
+
+print("Su edad es: ",res," Años")
---
+++ hello_Word.py
@@ -0,0 +1,6 @@
+def welcome_message():
+    name = input("ingresar su nombre: ")
+    print("Bienvenido", name, " al curso")
+    
+    if __name__ == "__main__":
+        welcome_message()
